1d552988d2e9e3c92c1d24f95ae74d522a207360
Jinpeng Zhu <dpinthinker@gmail.com>
2019-10-24 23:31:18 +0800

    update tfhub for tf2.0

--- tfhub/stylization.py
+++ tfhub/stylization.py
@@ -1,45 +1,67 @@
-from __future__ import absolute_import, division, print_function
+# Copyright 2019 dpthinker. All Rights Researved.
+# Changed for the book:  A Concise Handbook of TensorFlow 2.0
+#
+# Copyright 2019 The TensorFlow Hub Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
 
-import functools
-import os
-
-from matplotlib import gridspec
-
-import matplotlib.pylab as plt
+import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 import tensorflow_hub as hub
 
-# @title Load example images  { display-mode: "form" }
+def crop_center(image):
+    """Returns a cropped square image."""
+    shape = image.shape
+    new_shape = min(shape[1], shape[2])
+    offset_y = max(shape[1] - shape[2], 0) // 2
+    offset_x = max(shape[2] - shape[1], 0) // 2
+    image = tf.image.crop_to_bounding_box(image, offset_y, offset_x, new_shape, new_shape)
+    return image
 
-content_image_url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Golden_Gate_Bridge_from_Battery_Spencer.jpg/640px-Golden_Gate_Bridge_from_Battery_Spencer.jpg'  # @param {type:"string"}
-style_image_url = 'https://upload.wikimedia.org/wikipedia/commons/0/0a/The_Great_Wave_off_Kanagawa.jpg'  # @param {type:"string"}
-output_image_size = 384  # @param {type:"integer"}
+def load_image_local(image_path, image_size=(512, 512), preserve_aspect_ratio=True):
+    """Loads and preprocesses images."""
+    # Load and convert to float32 numpy array, add batch dimension, and normalize to range [0, 1].
+    img = plt.imread(image_path).astype(np.float32)[np.newaxis, ...]
+    if img.max() > 1.0:
+        img = img / 255.
+    if len(img.shape) == 3:
+        img = tf.stack([img, img, img], axis=-1)
+    img = crop_center(img)
+    img = tf.image.resize(img, image_size, preserve_aspect_ratio=True)
+    return img
 
-# The content image size can be arbitrary.
-content_img_size = (output_image_size, output_image_size)
-# The style prediction model was trained with image size 256 and it's the
-# recommended image size for the style image (though, other sizes work as
-# well but will lead to different results).
-style_img_size = (256, 256)  # Recommended to keep it at 256.
+def show_image(image, title):
+    plt.imshow(image, aspect='equal')
+    plt.axis('off')
+    plt.title(title)
+    plt.show()
 
-content_image = load_image(content_image_url, content_img_size)
-style_image = load_image(style_image_url, style_img_size)
-style_image = tf.nn.avg_pool(style_image, ksize=[3,3], strides=[1,1], padding='SAME')
-show_n([content_image, style_image], ['Content image', 'Style image'])
+content_image_path = "images/contentimg.jpeg"
+style_image_path = "images/styleimg5.jpeg"
 
-# Convert to float32 numpy array, add batch dimension, and normalize to range [0, 1]. Example using numpy:
-content_image = content_image.astype(np.float32)[np.newaxis, ...] / 255.
-style_image = style_image.astype(np.float32)[np.newaxis, ...] / 255.
-# Optionally resize the images. It is recommended that the style image is about
-# 256 pixels (this size was used when training the style transfer network).
-# The content image can be any size.
-style_image = tf.image.resize(style_image, (256, 256))
+content_image = load_image_local(content_image_path)
+style_image = load_image_local(style_image_path)
+
+show_image(content_image[0], "Content Image")
+show_image(style_image[0], "Style Image")
 
 # Load image stylization module.
-hub_module = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
+hub_module = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2');
 
 # Stylize image.
 outputs = hub_module(tf.constant(content_image), tf.constant(style_image))
 stylized_image = outputs[0]
-show_n([content_image, style_image, stylized_image], titles=['Original content image', 'Style image', 'Stylized image'])
+
+show_image(stylized_image[0], "Stylized Image")
